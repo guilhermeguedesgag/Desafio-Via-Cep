765e15edd5600458b9ec9ccbd281b1540a1621c5
guilhermewilhelm <guilhermeguedes.gag@gmail.com>
2024-07-08 02:47:59 -0300

    imprimindo cep e json anexado

--- src/BuscaCepNaApi.java
+++ src/BuscaCepNaApi.java
@@ -1,7 +1,112 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import modelo.cep.FormatoPadraoCep;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class BuscaCepNaApi {
 
-    public void buscaNaApi(){
+    public void buscaNaApi(String cep){
+        // na String Cep temos o CEP que foi informado
+        //na classe anterior
+        System.out.println("Imprimindo o cep:" + cep);
+
+
+
+    var resultado = cep ;
+
+
+    // teste para ver se extá printando
+        System.out.println("Aqui imprimiu o get Cep: " + resultado );
+
+
+        String enderecoApiCorreio = "https://viacep.com.br/ws/" + resultado + "/json/ ";
+
+    // até  aqui fui eu
+        try{
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(enderecoApiCorreio))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+
+
+            String json = response.body();
+            System.out.println(json);
+
+
+            //primeiro instancie o carinha responsável por buildar o json
+            JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+
+            //depois coloque uma condição para imprimir, somente se a response for false para pegarmos o erro.
+            if (jsonObject.has("Response")&& jsonObject.get("Response").getAsString().equals("False")){
+                System.out.println("Erro na busca, entrada não encontrada. "+ jsonObject.get("Error").getAsString());
+
+
+            }
+
+            //esse cara Gson abaixo irá buildar o json já validado acima
+            Gson gson = new GsonBuilder()
+                    .setPrettyPrinting().create();
+
+            FormatoPadraoCep formatoPadraoCep = gson.fromJson(json,FormatoPadraoCep.class);
+            System.out.println(formatoPadraoCep);
+
+
+            FileWriter cepEscrito =  new FileWriter("cep.txt");
+            cepEscrito.write(formatoPadraoCep.toString());
+            cepEscrito.close();
+
+
+        } catch (NumberFormatException | NullPointerException e) {
+            System.out.println("Aconteceu um erro: ");
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Algum erro de argumento na busca, verifique o endereço");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+//            FormatoPadraoCep formatoPadraoCep = gson.fromJson(enderecoApiCorreio, FormatoPadraoCep.class);
+//
+//            FormatoPadraoCep formatoPadraoCep = new FormatoPadraoCep();
+//
+//
+//            FileWriter escrita = new FileWriter("filmes.xlsx");
+//            escrita.write(meuTitulo.toString());
+//            escrita.close();
+
+
+
+
+        System.out.println("O programa finalizou corretamente!");
+        System.out.println("Endereço gerado da Api: "+ enderecoApiCorreio);
+        System.out.println("String: cep"+ cep);
+        System.out.println("String: resultado " + resultado);
+    }
+
+
+
+
 
 
     }
-}
+
+
+
+
+
--- src/BuscaInicial.java
+++ src/BuscaInicial.java
@@ -2,74 +2,96 @@
     import java.util.Scanner;
 
     public class BuscaInicial {
-        Scanner cepScanner = new Scanner(System.in);
-        Scanner respostaScanner = new Scanner(System.in);
-        private String respostaEscolha;
+        Scanner scanner = new Scanner(System.in);
+
+        public String respostaEscolha;
+        String cepProcurado="";
 
 
         public void iniciais() {
             //primeira frase do projeto
-            System.out.println("Digite o CEP que deseja procurar: ");
+            System.out.println("\nDigite o CEP que deseja procurar: ");
             iniciaisDois();
 
+        }
+
+        public Scanner getCepScanner() {
+            return scanner;
+        }
+
+        public String getCepProcurado() {
+            return cepProcurado;
         }
 
         public void iniciaisDois() {
             // cara responsável pela entrada de dados
 
-            String cepProcurado = cepScanner.nextLine();
+            while (true) {
+                // ao invés de colocar métodos de retornos, inseri
+                //um while para scanner's não ficarem como null
+                cepProcurado = scanner.nextLine();
 
-            if (cepProcurado.length() > 8) {
-                System.out.println("Ops...você digitou uma quantidade de caracteres a mais, digite \n" +
-                        "novamente apenas 8 dígitos: ");
-                iniciaisDois();
+                if (cepProcurado.length() > 8) {
+                    System.out.println("\nOps...você digitou uma quantidade de caracteres a mais, digite \n" +
+                            "novamente apenas 8 dígitos: ");
+                    System.out.println();
+                    System.out.println();
+                } else if (cepProcurado.length() < 8) {
+                    System.out.println("""
+                              \nOps...você digitou uma quantidade de caracteres menor
+                              do que a necessária para buscar o CEP, digite novamente
+                              apenas 8 dígitos:
+                            """);
 
-            } else if (cepProcurado.length() < 8) {
-                System.out.println(""" 
-                        
-                        Ops...você digitou uma quantidade de caracteres menor
-                        do que a necessária para buscar o CEP, digite novamente
-                        apenas 8 dígitos:                             
-                                
-                           
-                       """);
-                iniciaisDois();
-
-            } else {
-
-                System.out.println("Você digitou o CEP: " + cepProcurado + ", o CEP está correto?");
-
-
-
-                do {
-
-                    System.out.println("Digite 'Sim' ou 'Não': ");
-                    respostaEscolha = respostaScanner.nextLine();
-
-                    if (!respostaEscolha.equalsIgnoreCase("Sim") && (!respostaEscolha.equalsIgnoreCase("Não"))) {
-
-
-                        System.out.println("Ops...não entendi...digite 'Sim' ou 'Não':");
-                        respostaScanner.nextLine();
-                    } else {
-                        break;
-                    }
-                } while (!respostaEscolha.equalsIgnoreCase("Sim") && (!respostaEscolha.equalsIgnoreCase("Não")));
-
-
-                    if (respostaEscolha.equalsIgnoreCase("Sim")){
-
-                        BuscaCepNaApi buscaCepNaApi = new BuscaCepNaApi();
-                        buscaCepNaApi.buscaNaApi();
-
-                    } else if (respostaEscolha.equalsIgnoreCase("Não")) {
-                        System.out.println("Digite o CEP novamente: ");
-                        iniciaisDois();
-                    }
+                } else {
+                    System.out.println("\nVocê digitou o CEP: " + cepProcurado + ", o CEP está correto? ");
 
                 }
+                break;
+
+
+            }
+
+
+            do {
+
+                System.out.println("\nDigite 'Sim' ou 'Não': ");
+                respostaEscolha = scanner.nextLine();
+
+                if (!respostaEscolha.equalsIgnoreCase("Sim") && (!respostaEscolha.equalsIgnoreCase("Não"))) {
+
+
+                    System.out.println("\nOps...não entendi...digite 'Sim' ou 'Não':");
+
+                } else {
+                    break;
+                }
+            } while (!respostaEscolha.equalsIgnoreCase("Sim") && (!respostaEscolha.equalsIgnoreCase("Não")));
+
+
+            if (respostaEscolha.equalsIgnoreCase("Sim")) {
+
+                BuscaCepNaApi buscaCepNaApi = new BuscaCepNaApi();
+
+                //passei como parametro o 'cepProcurado' para ele armazenar
+                buscaCepNaApi.buscaNaApi(cepProcurado);
+
+            } else if (respostaEscolha.equalsIgnoreCase("Não")) {
+                System.out.println("\nDigite o CEP novamente: ");
+                iniciaisDois();
+            }
+
         }
+
+
+
+
+
+
+
     }
+
+
 
 
 
